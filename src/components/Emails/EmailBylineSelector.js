3c65ba6cfd161584bd08c95b457571de44fe43f9
snozza <andrewcsnead@gmail.com>
2017-05-15 17:22:21 +0100

    debounce switch due to freezing

--- src/components/Emails/EmailBylineSelector.js
+++ src/components/Emails/EmailBylineSelector.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import { Col, ControlLabel, FormGroup } from 'react-bootstrap';
+import { Col, ControlLabel, FormGroup, FormControl } from 'react-bootstrap';
 import Switch from 'react-bootstrap-switch';
 import DateTime from 'react-datetime';
 import moment from 'moment';
 import Select from 'react-select';
+import debounce from 'lodash/debounce';
 import renderTemplate from 'helpers/renderTemplate';
 
 import bylineTemplate from './templates/byline.html';
 import './EmailBylineSelector.css';
 
 export default class EmailBylineSelector extends Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleGuest =
+      debounce((field) => this._handleToggleGuest(field), 500, { leading: true });
+    this.state = {
+      guest: props.value.guest
+    };
+  }
+
   get authors() {
     const { authors } = this.props;
     return authors.map((a) => ({
@@ -33,6 +43,24 @@ export default class EmailBylineSelector extends Component {
     onChange(author);
   }
 
+  _handleToggleGuest(field) {
+    this.setState({ guest: !this.state.guest });
+    const { onChange } = this.props;
+    const author = {};
+    const guestVal = { ...field.selected.value };
+    guestVal.sendDate = field.sendDate || '';
+    if (!field.guest) {
+      guestVal.photo = '';
+      guestVal.authorURL = '';
+      author.guest = { name: guestVal.name };
+      author.htmlBody = renderTemplate(bylineTemplate, guestVal);
+    } else {
+      author.guest = null;
+      author.htmlBody = renderTemplate(bylineTemplate, guestVal);
+    }
+    onChange(author);
+  }
+
   handleAuthorChange(field, selected) {
     const { onChange } = this.props;
     const author = { selected: null, htmlBody: '' };
@@ -47,6 +75,7 @@ export default class EmailBylineSelector extends Component {
 
   render() {
     const { value } = this.props;
+    const defaultAuthors = !this.state.guest;
     const selectedAuthor = {
       label: value.selected && value.selected.label,
       value: value.selected && value.selected
@@ -57,20 +86,22 @@ export default class EmailBylineSelector extends Component {
           <ControlLabel>Default</ControlLabel>
           <Switch
             wrapperClass="bootstrap-switch-byline"
-            state
-            onChange={() => {}}
+            state={defaultAuthors}
+            onChange={() => this.handleToggleGuest(value)}
           />
         </Col>
         <Col sm={5} md={5} lg={5}>
           <ControlLabel>Author</ControlLabel>
-          <Select
-            name="author-select"
-            placeholder="Author..."
-            options={this.authors}
-            autosize={false}
-            value={selectedAuthor.value && selectedAuthor}
-            onChange={(selected) => this.handleAuthorChange(value, selected)}
-          />
+          {defaultAuthors
+            ? <Select
+              name="author-select"
+              placeholder="Author..."
+              options={this.authors}
+              autosize={false}
+              value={selectedAuthor.value && selectedAuthor}
+              onChange={(selected) => this.handleAuthorChange(value, selected)}
+            />
+            : <FormControl type="text" placeholder="Name" />}
         </Col>
         {selectedAuthor.label &&
           <Col sm={5} md={4} lg={4}>
