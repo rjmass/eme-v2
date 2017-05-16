be258e1e440d681fc66e31d071e65a4a1e09c0e5
snozza <andrewcsnead@gmail.com>
2017-05-16 12:07:24 +0100

    update switch to new version and update signature

--- src/components/Emails/EmailBylineSelector.js
+++ src/components/Emails/EmailBylineSelector.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Col, ControlLabel, FormGroup, FormControl } from 'react-bootstrap';
+import { Col, Row, ControlLabel, FormGroup, FormControl } from 'react-bootstrap';
 import Switch from 'react-bootstrap-switch';
 import DateTime from 'react-datetime';
 import moment from 'moment';
@@ -15,9 +15,10 @@ export default class EmailBylineSelector extends Component {
     super(props);
     this.handleToggleGuest =
       debounce((field) => this._handleToggleGuest(field), 500, { leading: true });
-    this.state = {
-      guest: props.value.guest
-    };
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return (!(nextProps.value === this.props.value));
   }
 
   get authors() {
@@ -44,7 +45,6 @@ export default class EmailBylineSelector extends Component {
   }
 
   _handleToggleGuest(field) {
-    this.setState({ guest: !this.state.guest });
     const { onChange } = this.props;
     const author = {};
     const guestVal = { ...field.selected.value };
@@ -52,7 +52,7 @@ export default class EmailBylineSelector extends Component {
     if (!field.guest) {
       guestVal.photo = '';
       guestVal.authorURL = '';
-      author.guest = { name: guestVal.name };
+      author.guest = { name: guestVal.name, photo: '', authorURL: '' };
       author.htmlBody = renderTemplate(bylineTemplate, guestVal);
     } else {
       author.guest = null;
@@ -73,49 +73,74 @@ export default class EmailBylineSelector extends Component {
     onChange(author);
   }
 
+  handleGuestChange(field, guest) {
+    const { onChange } = this.props;
+    const author = {};
+    const newValue = { ...field.guest, ...guest };
+    newValue.sendDate = field.sendDate || '';
+    author.guest = guest;
+    author.htmlBody = renderTemplate(bylineTemplate, newValue);
+    onChange(author);
+  }
+
   render() {
     const { value } = this.props;
-    const defaultAuthors = !this.state.guest;
+    const defaultAuthors = !value.guest;
     const selectedAuthor = {
       label: value.selected && value.selected.label,
       value: value.selected && value.selected
     };
     return (
-      <FormGroup>
-        <Col sm={2} md={3} lg={3}>
-          <ControlLabel>Default</ControlLabel>
-          <Switch
-            wrapperClass="bootstrap-switch-byline"
-            state={defaultAuthors}
-            onChange={() => this.handleToggleGuest(value)}
-          />
-        </Col>
-        <Col sm={5} md={5} lg={5}>
-          <ControlLabel>Author</ControlLabel>
-          {defaultAuthors
-            ? <Select
-              name="author-select"
-              placeholder="Author..."
-              options={this.authors}
-              autosize={false}
-              value={selectedAuthor.value && selectedAuthor}
-              onChange={(selected) => this.handleAuthorChange(value, selected)}
-            />
-            : <FormControl type="text" placeholder="Name" />}
-        </Col>
-        {selectedAuthor.label &&
-          <Col sm={5} md={4} lg={4}>
-            <ControlLabel>Send Date</ControlLabel>
-            <DateTime
-              defaultValue={new Date()}
-              dateFormat="LL"
-              timeFormat={false}
-              isValidDate={this.isValidDate}
-              onChange={(_date) => this.handleDateSelect(value, _date)}
-            />
+      <div>
+        <Row>
+          <Col xs={12}>
+            <FormGroup>
+              <Col sm={2} md={3} lg={3}>
+                <ControlLabel>Default</ControlLabel>
+                <Switch
+                  value={defaultAuthors}
+                  wrapperClass="bootstrap-switch-byline"
+                  onChange={() => this.handleToggleGuest(value)}
+                />
+              </Col>
+              <Col sm={5} md={5} lg={5}>
+                <ControlLabel>Author</ControlLabel>
+                {defaultAuthors
+                  ? <Select
+                    name="author-select"
+                    placeholder="Author..."
+                    options={this.authors}
+                    autosize={false}
+                    value={selectedAuthor.value && selectedAuthor}
+                    onChange={(selected) => this.handleAuthorChange(value, selected)}
+                  />
+                  : <FormControl
+                    type="text"
+                    placeholder="Name"
+                    onChange={(e) => this.handleGuestChange(value, e.target.value)}
+                  />}
+              </Col>
+                {!defaultAuthors || selectedAuthor.label &&
+                  <Col sm={5} md={4} lg={4}>
+                    <ControlLabel>Send Date</ControlLabel>
+                    <DateTime
+                      defaultValue={new Date()}
+                      dateFormat="LL"
+                      timeFormat={false}
+                      isValidDate={this.isValidDate}
+                      onChange={(_date) => this.handleDateSelect(value, _date)}
+                    />
+                  </Col>
+                  }
+            </FormGroup>
           </Col>
-        }
-      </FormGroup>
+        </Row>
+        <Row>
+          <Col>
+            Hello
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
